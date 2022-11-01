34cd213c7c57ba50d761a4ae094a95e8d6ba5bfc
Mirzabek <mirzabekmirzayev746@gmail.com>
2022-11-01 15:30:34 +0500

    Initial Commit

---
+++ ios_instaClone/ios_instaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_instaCloneApp.swift
+//  ios_instaClone
+//
+//  Created by Mirzabek on 01/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_instaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
